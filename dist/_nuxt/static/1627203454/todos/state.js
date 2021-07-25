2db0f99dbc590f551449940ab0115a61b055e3f5
yozakura <matchnokaori@gmail.com>
2021-07-25 18:44:01 +0900

    fix: .gitignoreからdistを削除

---
+++ dist/_nuxt/static/1627203454/todos/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627203454",layout:"default",error:a,state:{todos:{list:[]}},serverRendered:true,routePath:"\u002Ftodos",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
